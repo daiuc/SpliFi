ab7740d1da63f2ad3becacb12fbba2c37dacccde
daiuc <chaodai@uchicago.edu>
2024-08-15 16:36:21 -0500

    fixing bug when qvalue function fails
    qvalue requires certain number of pvalues, if not enough it fails and need to manually set lambda. here if it fails manually set it to 0.

--- code/workflow/scripts/AddQvalueToQTLtoolsOutput.R
+++ code/workflow/scripts/AddQvalueToQTLtoolsOutput.R
@@ -2,12 +2,17 @@
 
 # written by bjf79
 
-#Use hard coded arguments in interactive R session, else use command line args
-if (interactive()){
-  args <- scan(text=
-                 "/project2/yangili1/cdai/mRNA-editing/YRI/Results/hs38/edQTL_FixedPCs/cis_10000/permutations_pass_all_chr.txt.gz scratch/Qvals.txt.gz   ", what='character')
-} else{
-  args <- commandArgs(trailingOnly=TRUE)
+# Use hard coded arguments in interactive R session, else use command line args
+if (interactive()) {
+    args <- scan(
+        text =
+            "/project/yangili1/cdai/SpliFi/code/results/eqtl/GTEx/Testis/perm/chr18.txt scratch/Qvals.txt.gz   ", what = "character"
+    )
+} else {
+    args <- commandArgs(trailingOnly = TRUE)
+    if (length(args) < 2) {
+        stop("Rscript AddQvalueToQTLtoolsOutput.R <input file> <output file>")
+    }
 }
 
 FileIn <- args[1]
@@ -17,25 +22,32 @@ suppressMessages(library(data.table))
 suppressMessages(library(qvalue))
 
 
-data.in = fread(FileIn)
+data.in <- fread(FileIn)
 
 if (ncol(data.in) == 20) { # QTLtools 1.3 output
-  cnames <- c(
-    "phenotype_id", "phenotype_chr", "phenotype_start", "phenotype_end",
-    "phenotype_strand", "num_variants", "best_nom_dist", "best_genotype_id",
-    "best_genotype_chr", "best_genotype_start", "best_genotype_end", "dof_true",
-    "dof_est", "beta_ml1", "beta_ml2", "pval_nom",
-    "pval_r2", "slope", "pval_emp", "pval_adj")
-  names(data.in) <- cnames
+    cnames <- c(
+        "phenotype_id", "phenotype_chr", "phenotype_start", "phenotype_end",
+        "phenotype_strand", "num_variants", "best_nom_dist", "best_genotype_id",
+        "best_genotype_chr", "best_genotype_start", "best_genotype_end", "dof_true",
+        "dof_est", "beta_ml1", "beta_ml2", "pval_nom",
+        "pval_r2", "slope", "pval_emp", "pval_adj"
+    )
+    names(data.in) <- cnames
 } else if (ncol(data.in) == 19) { # QTLtools 1.2 output
-  cnames <- c(
-    "phenotype_id", "phenotype_chr", "phenotype_start", "phenotype_end",
-    "phenotype_strand", "num_variants", "best_nom_dist", "best_genotype_id",
-    "best_genotype_chr", "best_genotype_start", "best_genotype_end", "dof_true",
-    "dof_est", "beta_ml1", "beta_ml2", "pval_nom",
-    "slope", "pval_emp", "pval_adj")
-  names(data.in) <- cnames
+    cnames <- c(
+        "phenotype_id", "phenotype_chr", "phenotype_start", "phenotype_end",
+        "phenotype_strand", "num_variants", "best_nom_dist", "best_genotype_id",
+        "best_genotype_chr", "best_genotype_start", "best_genotype_end", "dof_true",
+        "dof_est", "beta_ml1", "beta_ml2", "pval_nom",
+        "slope", "pval_emp", "pval_adj"
+    )
+    names(data.in) <- cnames
 }
 
-data.in$q <- signif(qvalue(data.in$pval_adj)$qvalues, 5)
+q <- try(qvalue(data.in$pval_adj), silent = TRUE)
+if (inherits(q, "try-error")) {
+    message("Setting qvalue parameter lambda=0 and rerun qvalue.")
+    q <- qvalue(data.in$pval_adj, lambda = 0)
+}
+data.in$q <- signif(q$qvalues, 5)
 fwrite(na.omit(data.in), FileOut, sep = " ")
